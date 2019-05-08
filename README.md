eec4b7f82992c1eb81414d11e98e0ea91f237654
Luke Herrington <lherrington100@gmail.com>
2019-05-08 11:56:07 +0545

    add preview env var in config

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Lauren Herrington Blog
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3afa0cba-45dd-4a94-bae7-71a01582937f/deploy-status)](https://app.netlify.com/sites/affectionate-bose-009344/deploys)
 
 ## Setup
 * `yarn`
--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,10 @@ module.exports = {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken:
+          process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN ||
+          process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST,
       },
     },
     {
