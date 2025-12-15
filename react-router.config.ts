4fcea1d50ed0bb1d575fa1a28ab9a43d52f1dea1
Luke Herrington <luke@apella.io>
2025-12-15 13:28:16 -0800

    Enable pre-render for home page

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   ssr: true,
+  prerender: ["/"],
   future: {
     v8_viteEnvironmentApi: true,
     v8_splitRouteModules: true,
