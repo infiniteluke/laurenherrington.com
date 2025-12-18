557117ab66d70cde6c58100a4e3b216664c0f765
Luke Herrington <luke@apella.io>
2025-12-17 17:57:07 -0800

    make portfolio item a link

--- app/components/PortfolioItem.tsx
+++ app/components/PortfolioItem.tsx
@@ -4,7 +4,9 @@ import { Link } from "react-router";
 export function PortfolioItem({ product }: { product: Listing }) {
   return (
     <div className="flex flex-col gap-2 break-inside-avoid md:m-2">
-      <h3>{product.title}</h3>
+      <Link to={`/item/${product.id}`}>
+        <h3 className="underline">{product.title}</h3>
+      </Link>
       <Link
         to={`/item/${product.id}`}
         className="overflow-hidden bg-neutral-100"
